00985633c6d97ca2b2505de1fdaa49a3c56d8fc8
Takatoshi Maeda <maeda@labit.com>
2013-04-30 11:18:09 +0900

    add XQuartz install path

--- recipes/xquartz.rb
+++ recipes/xquartz.rb
@@ -2,6 +2,7 @@ include_recipe 'dmg'
 
 dmg_package "XQuartz" do
   volumes_dir "XQuartz-2.7.4"
+  destination "/Applications/Utilities"
   source "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.4.dmg"
   type "pkg"
 end
